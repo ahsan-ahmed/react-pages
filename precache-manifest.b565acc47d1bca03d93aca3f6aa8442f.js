94937bae448b44e4a372a9d5e413facfcf7b85c7
ahsan-ahmed <ahsan_ahmed01@yahoo.com>
2019-02-05 13:44:51 +0500

    Updates

---
+++ precache-manifest.b565acc47d1bca03d93aca3f6aa8442f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "36215941b2a6f15fd2da",
+    "url": "/react-pages/static/css/main.0f6103de.chunk.css"
+  },
+  {
+    "revision": "36215941b2a6f15fd2da",
+    "url": "/react-pages/static/js/main.36215941.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/react-pages/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "c9847d069c9cae9de2c6",
+    "url": "/react-pages/static/js/runtime~main.c9847d06.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "18968d852154f3990a30fa7d476ac179",
+    "url": "/react-pages/index.html"
+  }
+];
